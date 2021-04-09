e0e23f76e355ddec5568c3fe90a35112202ec152
MoooooN <1729310316@qq.com>
2021-04-09 20:33:03 +0800

    router

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import App from './app/App'
-import router from './router'
+import {router} from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VeeValidate from 'vee-validate'
