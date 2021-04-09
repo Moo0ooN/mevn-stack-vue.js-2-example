6eff1cc60e269a6b259fc2a4939021ec4bb045d6
MoooooN <1729310316@qq.com>
2021-04-09 15:31:31 +0800

    modify App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <template>
-  <b-container>
-    <h1>MEVN Stack</h1>
-    <router-view/>
-  </b-container>
+  <div class="jumbotron">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6 offset-sm-3">
+          <div v-if="alert.message" :class="`alert ${alert.type}`">{{alert.message}}</div>
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
-</script>
+  import { mapState, mapActions } from 'vuex'
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 30px;
-}
-</style>
+  export default {
+    name: 'app',
+    computed: {
+      ...mapState({
+        alert: state => state.alert
+      })
+    },
+    methods: {
+      ...mapActions({
+        clearAlert: 'alert/clear'
+      })
+    },
+    watch: {
+      $route (to, from){
+        // clear alert on location change
+        this.clearAlert();
+      }
+    }
+  };
+</script>
