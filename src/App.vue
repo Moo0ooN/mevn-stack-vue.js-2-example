1001402202931091acfc00f044a51f96d0ec0ef0
MoooooN <1729310316@qq.com>
2021-04-09 18:56:13 +0800

    message

--- src/App.vue
+++ src/App.vue
@@ -25,10 +25,11 @@
   <div class="jumbotron">
     <div class="container">
       <div class="row">
-        <div class="col-sm-6 offset-sm-3">
-          <div v-if="alert.message" :class="`alert ${alert.type}`">{{alert.message}}</div>
-          <router-view></router-view>
-        </div>
+        <router-view></router-view>
+<!--        <div class="col-sm-6 offset-sm-3">-->
+<!--          <div v-if="alert.message" :class="`alert ${alert.type}`">{{alert.message}}</div>-->
+<!--          <router-view></router-view>-->
+<!--        </div>-->
       </div>
     </div>
   </div>
