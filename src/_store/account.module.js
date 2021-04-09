e28beeb4626db78f28582cc55650563ee6230ccd
MoooooN <1729310316@qq.com>
2021-04-09 23:50:28 +0800

    login

--- src/_store/account.module.js
+++ src/_store/account.module.js
@@ -37,7 +37,7 @@ const actions = {
           router.push('/login')
           setTimeout(() => {
             // display success message after route change completes
-            dispatch('alert/success', 'Registration successful', { root: true })
+            dispatch('alert/success', '注册成功', { root: true })
           })
         },
         error => {
