1352096fac7200a20454a2fd4c988a5ba714821c
MoooooN <1729310316@qq.com>
2021-04-09 19:00:20 +0800

    message

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,61 @@
-<!--<template>-->
-<!--  <b-container>-->
-<!--    <h1>MEVN Stack</h1>-->
-<!--    <router-view/>-->
-<!--  </b-container>-->
-<!--</template>-->
-
-<!--<script>-->
-<!--export default {-->
-<!--  name: 'app'-->
-<!--}-->
-<!--</script>-->
-
-<!--<style>-->
-<!--#app {-->
-<!--  font-family: 'Avenir', Helvetica, Arial, sans-serif;-->
-<!--  -webkit-font-smoothing: antialiased;-->
-<!--  -moz-osx-font-smoothing: grayscale;-->
-<!--  color: #2c3e50;-->
-<!--  margin-top: 30px;-->
-<!--}-->
-<!--</style>-->
-
 <template>
-  <div class="jumbotron">
-    <div class="container">
-      <div class="row">
-        <router-view></router-view>
+  <b-container>
+    <h1>MEVN Stack</h1>
+    <router-view/>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 30px;
+}
+</style>
+
+<!--<template>-->
+<!--  <div class="jumbotron">-->
+<!--    <div class="container">-->
+<!--      <div class="row">-->
+<!--        <router-view></router-view>-->
 <!--        <div class="col-sm-6 offset-sm-3">-->
 <!--          <div v-if="alert.message" :class="`alert ${alert.type}`">{{alert.message}}</div>-->
 <!--          <router-view></router-view>-->
 <!--        </div>-->
-      </div>
-    </div>
-  </div>
-</template>
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
 
-<script>
-  // import { mapState, mapActions } from 'vuex'
-  import { mapActions } from 'vuex'
+<!--<script>-->
+<!--  // import { mapState, mapActions } from 'vuex'-->
+<!--  import { mapActions } from 'vuex'-->
 
-  export default {
-    name: 'app',
-    // computed: {
-    //   ...mapState({
-    //     alert: state => this.state.alert,
-    //   })
-    // },
-    methods: {
-      ...mapActions({
-        clearAlert: 'alert/clear'
-      })
-    },
-    watch: {
-      $route (to, from) {
-        // clear alert on location change
-        this.clearAlert()
-      }
-    }
-  }
-</script>
+<!--  export default {-->
+<!--    name: 'app',-->
+<!--    computed: {-->
+<!--      ...mapState({-->
+<!--        alert: state => this.state.alert,-->
+<!--      })-->
+<!--    },-->
+<!--    methods: {-->
+<!--      ...mapActions({-->
+<!--        clearAlert: 'alert/clear'-->
+<!--      })-->
+<!--    },-->
+<!--    watch: {-->
+<!--      $route (to, from) {-->
+<!--        // clear alert on location change-->
+<!--        this.clearAlert()-->
+<!--      }-->
+<!--    }-->
+<!--  }-->
+<!--</script>-->
