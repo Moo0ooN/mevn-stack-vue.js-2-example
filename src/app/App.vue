cf2802e00654bc85e40575a9631ca1d765581afc
MoooooN <1729310316@qq.com>
2021-04-10 14:01:36 +0800

    modify the App.vue

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,9 +1,10 @@
-<!--<template>-->
-<!--  <b-container>-->
+<template>
+  <b-container>
 <!--    <h1>MEVN Stack</h1>-->
-<!--    <router-view/>-->
-<!--  </b-container>-->
-<!--</template>-->
+    <div v-if="alert.message" :class="`alert ${alert.type}`">{{alert.message}}</div>
+    <router-view/>
+  </b-container>
+</template>
 
 <!--<script>-->
 <!--export default {-->
@@ -11,28 +12,20 @@
 <!--}-->
 <!--</script>-->
 
-<!--<style>-->
-<!--#app {-->
-<!--  font-family: 'Avenir', Helvetica, Arial, sans-serif;-->
-<!--  -webkit-font-smoothing: antialiased;-->
-<!--  -moz-osx-font-smoothing: grayscale;-->
-<!--  color: #2c3e50;-->
-<!--  margin-top: 30px;-->
-<!--}-->
-<!--</style>-->
 
-<template>
-  <div class="jumbotron">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-6 offset-sm-3">
-          <div v-if="alert.message" :class="`alert ${alert.type}`">{{alert.message}}</div>
-          <router-view></router-view>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
+
+<!--<template>-->
+<!--  <div class="jumbotron">-->
+<!--    <div class="container">-->
+<!--      <div class="row">-->
+<!--        <div class="col-sm-6 offset-sm-3">-->
+<!--          <div v-if="alert.message" :class="`alert ${alert.type}`">{{alert.message}}</div>-->
+<!--          <router-view></router-view>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
 
 <script>
   import { mapState, mapActions } from 'vuex'
@@ -57,3 +50,13 @@
     }
   }
 </script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 30px;
+  }
+</style>
